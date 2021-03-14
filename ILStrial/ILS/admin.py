b9621e030116698542c18aa90c8276788612ffc8
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2021-03-15 03:35:43 +0800

    Update admin.py

--- ILStrial/ILS/admin.py
+++ ILStrial/ILS/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-#from .models import Book
-#from .models import Entry
- 
+from .models import Book
+
 # Register your models here.
-#admin.site.register(Book)
-#admin.site.register(Entry)
+admin.site.register(Book)
+admin.site.register(Entry)
